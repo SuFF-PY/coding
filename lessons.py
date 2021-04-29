0307790df3497fcf95cf3dfc6dd3af03b893caca
SuFF-PY <f_in_x@mail.ru>
2021-04-29 20:40:39 +0500

    understand more

--- lessons.py
+++ lessons.py
@@ -1,3 +1,12 @@
-print('1.3 * 0.3 = ', 1.3 * 0.3)
-print('1.3 * .3 = ', 1.3 * .3)
-print('279 * .105 = ', 279 * .105)
+# print('1.3 * 0.3 = ', 1.3 * 0.3)
+# print('1.3 * .3 = ', 1.3 * .3)
+# print('279 * .105 = ', 279 * .105)
+
+# print(3 == 3.0)
+# print(1 > 9)
+# print(3 <= 5)
+# print(False == 0)
+
+# print('True and False', True and False)
+# print('True or False', True or False)
+
